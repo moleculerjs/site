4308f6dd41d6711fa76eeaf1f0e3766bfb67705f
Icebob <mereg.norbert@gmail.com>
2017-05-22 13:38:42 +0200

    mobile-friendly

--- themes/moleculer/source/js/mobile_nav.js
+++ themes/moleculer/source/js/mobile_nav.js
@@ -20,4 +20,29 @@
     e.preventDefault();
     body.classList.remove(CLASS_NAME);
   });
+
+  // Toggle sidebar on swipe
+  var start = {}, end = {}
+
+  document.body.addEventListener('touchstart', function (e) {
+    start.x = e.changedTouches[0].clientX
+    start.y = e.changedTouches[0].clientY
+  })
+
+  document.body.addEventListener('touchend', function (e) {
+    end.y = e.changedTouches[0].clientY
+    end.x = e.changedTouches[0].clientX
+
+    var xDiff = end.x - start.x
+    var yDiff = end.y - start.y
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+      if (xDiff > 0 && start.x <= 80) 
+        body.classList.toggle(CLASS_NAME);
+      else 
+        body.classList.remove(CLASS_NAME);
+    }
+  })
+
+
 })();
