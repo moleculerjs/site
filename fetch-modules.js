0e26b9c97179f09c42dec475337ccd3d1bf84c2b
AndMaz <andre.maz.90@gmail.com>
2019-05-20 20:12:26 +0100

    Test travis

---
+++ fetch-modules.js
@@ -0,0 +1,23 @@
+const rp = require("request-promise");
+// Travis don't like fsPromises
+// const fsPromises = require("fs").promises;
+const fs = require("fs");
+const util = require("util");
+const writeFile = util.promisify(fs.writeFile);
+
+const URL = `https://raw.githubusercontent.com/AndreMaz/awesome-moleculer/master/out/site_modules.yml`;
+
+const DIR_TO_WRITE = `source/_data/modules.yml`;
+
+async function fetchModules() {
+    try {
+        // HTTP GET yaml file as string
+        const payload = await rp.get(URL);
+
+        await writeFile(DIR_TO_WRITE, payload);
+    } catch (error) {
+        throw error;
+    }
+}
+
+fetchModules();
