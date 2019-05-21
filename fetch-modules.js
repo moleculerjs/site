3cd92b1a82e2683161e5eeed09e9569b6dbdb616
AndMaz <andre.maz.90@gmail.com>
2019-05-21 22:06:36 +0100

    Some docs

--- fetch-modules.js
+++ fetch-modules.js
@@ -4,20 +4,20 @@ const util = require("util");
 const writeFile = util.promisify(fs.writeFile);
 
 // URL pointing to new modules
-const URL = `https://raw.githubusercontent.com/AndreMaz/awesome-moleculer/master/out/site_modules.yml`;
+const URL = `https://raw.githubusercontent.com/moleculerjs/awesome-moleculer/master/out/site_modules.yml`;
 
 // Path to modules
 const DIR_TO_WRITE = `source/_data/modules.yml`;
 
 async function fetchModules() {
     try {
-        // HTTP GET yaml file as a string
+        // GET yaml file as a string
         const payload = await rp.get(URL);
 
         await writeFile(DIR_TO_WRITE, payload);
     } catch (error) {
         console.log(error);
-		process.exit(1);
+        process.exit(1);
     }
 }
 
