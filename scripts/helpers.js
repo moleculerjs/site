8f56198e8f37cfb3fdf66c8c4a8828e66caaff94
AndreMaz <andre.maz.90@gmail.com>
2019-11-14 22:18:34 +0000

    Hide message

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -69,7 +69,7 @@ hexo.extend.helper.register("isOlderVersion", function() {
     return (idx > latestIdx);
 });
 
-hexo.extend.helper.register("isCurrentVersion", function() {
+/*hexo.extend.helper.register("isCurrentVersion", function() {
 	var p = this.page.canonical_path.split('/');
 	if (p.length < 2) return false;
 	var type = p[0];
@@ -82,7 +82,7 @@ hexo.extend.helper.register("isCurrentVersion", function() {
 	const latestIdx = versions.findIndex(v => v == this.site.data.versions.latest);
 	
     return (idx === latestIdx);
-});
+});*/
 
 hexo.extend.helper.register("isNewerVersion", function() {
 	var p = this.page.canonical_path.split('/');
